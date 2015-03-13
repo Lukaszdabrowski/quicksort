bf7d9920c0832eb232e602b9c739c9c3325b1369
Lukaszdabrowski <ukasz.dabrowski@gmail.com>
2015-03-13 13:37:17 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdint.h>
 
 void quicksort(int [10],int,int);
 
@@ -21,6 +22,17 @@ int main(){
 
   return 0;
 }
+
+int64_t timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p)
+{
+  return ((timeA_p->tv_sec * 1000000000) + timeA_p->tv_nsec) -
+           ((timeB_p->tv_sec * 1000000000) + timeB_p->tv_nsec);
+}
+
+int main(int argc, char **argv)
+{
+  struct timespec start, end;
+  clock_gettime(CLOCK_MONOTONIC, &start);
 
 void quicksort(int x[10],int first,int last){
     int pivot,j,temp,i;
@@ -48,8 +60,12 @@ void quicksort(int x[10],int first,int last){
          quicksort(x,first,j-1);
          quicksort(x,j+1,last);
          
+         clock_gettime(CLOCK_MONOTONIC, &end);
+         uint64_t timeElapsed = timespecDiff(&end, &start);
+        }
     }
 }
+
 
 int timer(){
     clock_t start = clock();
